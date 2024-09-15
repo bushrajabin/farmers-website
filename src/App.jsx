799a39c1332595d09690eeed22734c1aace550ab
Bushra jabin <bushrabintjabin@gmail.com>
2024-09-15 16:31:54 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter,Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Login from "./components/LoginPage/Login";
 
 function App() {
-return(
-  <>
-  <BrowserRouter>
-<Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/login" element={<Login/>}/>
-</Routes>
-  </BrowserRouter>
-  </>
-)
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
